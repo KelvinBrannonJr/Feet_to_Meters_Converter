6b3101ad50a43591b6bb9adeccce153f2fc6807c
Kelvin <Kelzjr56@gmail.com>
2023-05-17 16:34:53 -0400

    Added clean exit, exception handling for both input values to be present

--- main.py
+++ main.py
@@ -10,27 +10,33 @@ inches_input = sg.Input(key='inches')
 convert_btn = sg.Button("Convert", key='Convert')
 result_output = sg.Text("", key='output')
 
-window = sg.Window(
-            title="Feet To Meters Converter",
-            layout=[
-                [feet_label, feet_input],
-                [inches_label, inches_input],
-                [convert_btn, result_output]
-            ],
-)
+exit_btn = sg.Button("Exit")
+
+sg.theme('Black')
+
+layout = [
+            [feet_label, feet_input],
+            [inches_label, inches_input],
+            [convert_btn, exit_btn, result_output],
+        ]
+
+window = sg.Window(title="Feet To Meters Converter", layout=layout)
 
 while True:
     event, values = window.read()
-    print(event, values)
-
-    if event == "Convert":
-        feet = float(values['feet'])
-        inches = float(values['inches'])
-        result = convert(feet, inches)
-        window['output'].update(value=f"{result} meters", text_color="white")
-
-    if event == sg.WIN_CLOSED:
+    if event == sg.WIN_CLOSED or event == "Exit":
         break
+    if event == "Convert":
+        try:
+            feet = float(values['feet'])
+            inches = float(values['inches'])
+            result = convert(feet, inches)
+            window['output'].update(value=f"{result} meters", text_color="white")
+            window['feet'].Update(value="")
+            window['inches'].Update(value="")
+
+        except ValueError:
+            sg.popup("Please provide two numbers.")
 
 window.close()
 
